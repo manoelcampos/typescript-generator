fbe774c49fef9b9193aba119f60f6dbd2b8a80ae
Konrad Kügler <swamblumat-git@yahoo.de>
2026-02-08 13:51:36 +0100

    build: Gradle infrastructure
    - switch from Maven to Gradle for building the Gradle plugin
    - enable Gradle caching

--- sample-gradle-spring/src/main/java/cz/habarta/typescript/generator/sample/spring/SpringTestApplication.java
+++ sample-gradle-spring/src/main/java/cz/habarta/typescript/generator/sample/spring/SpringTestApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 public class SpringTestApplication {
 
@@ -23,7 +22,7 @@ public class SpringTestApplication {
         private final AtomicLong counter = new AtomicLong();
 
         @RequestMapping("/greeting")
-        public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
             return new Greeting(counter.incrementAndGet(), String.format(template, name));
         }
 
